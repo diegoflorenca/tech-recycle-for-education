68ec6ac8d51e37244255f185416261dc3d4e6103
Diego Felipe Florença <diegoflorenca@gmail.com>
2020-09-05 23:40:59 -0300

    Created Select component with react-select

---
+++ src/components/Select/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import Select from 'react-select';
+
+const options = [
+  { value: 'laptop', label: 'Laptop' },
+  { value: 'computer', label: 'Computer' },
+  { value: 'phone', label: 'Phone' },
+  { value: 'accessories', label: 'Accessories' },
+  { value: 'cables', label: 'Cables' },
+  { value: 'hardware', label: 'Hardware' },
+];
+
+function select(props) {
+  return (
+    <Select
+      isMulti
+      name="items"
+      options={options}
+      isSearchable={false}
+      placeholder={props.placeHolder}
+      theme={(theme) => ({
+        ...theme,
+        padding: '56px',
+        borderRadius: '4px',
+        colors: {
+          ...theme.colors,
+          primary50: '#f8f8f8',
+          primary25: '#f4f4f4',
+          primary: '#65068a',
+        },
+      })}
+    />
+  );
+}
+
+export default select;
+
+// Overriding the theme
